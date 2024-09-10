062f345b60ceb636e2851e51d636209380f4c9f9
Ted Rainger <edward.rainger@gmail.com>
2024-09-10 14:04:45 +1200

    Set up Auth

--- client/index.tsx
+++ client/index.tsx
@@ -1,16 +1,27 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import App from './components/App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import routes from './routes'
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter(routes)
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ClerkProvider>,
   )
 })
