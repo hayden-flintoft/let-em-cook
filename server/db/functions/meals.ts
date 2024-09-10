051b33d4996d68b82afb66466dc1c3f2fb91d10b
karl-bloomfield <171457775+karl-bloomfield@users.noreply.github.com>
2024-09-10 15:58:47 +1200

    Create serverside routes

---
+++ server/db/functions/meals.ts
@@ -0,0 +1,4 @@
+// import connection from '../connections'
+// import { Meal } from '../../../models/meals'
+
+// const db = connection
---
+++ server/routes/users.ts
@@ -0,0 +1,89 @@
+import express from 'express'
+import * as db from '../db/functions/users'
+
+const router = express.Router()
+
+// Get '/api/vi/users'
+router.get('/user/', async (req, res) => {
+  try {
+    const users = await db.getAllUsers()
+    res.json(users)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+// Get '/api/vi/users/:id'
+router.get('/user/:id', async (req, res) => {
+  const username = Number(req.params.id)
+  try {
+    const user = await db.getUserById(username)
+    res.json(user)
+  } catch {
+    // console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+// Get '/api/vi/users/:username'
+router.get('/user/:username', async (req, res) => {
+  const username = Number(req.params.username)
+  try {
+    const user = await db.getUserById(username)
+    res.json(user)
+  } catch {
+    // console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+// Get '/api/vi/users/:email'
+router.get('/user/:email', async (req, res) => {
+  const email = String(req.params.email)
+  try {
+    const user = await db.getUserByEmail(email)
+    res.json(user)
+  } catch {
+    // console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+// Post '/api/vi/users'
+router.post('/user/', async (req, res) => {
+  const { username, firstName, lastName, email, authToken } = req.body
+  try {
+    const user = await db.createUser({
+      username,
+      firstName,
+      lastName,
+      email,
+      authToken,
+    })
+    res.json(user)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+// Patch '/api/vi/users/:id'
+router.patch('/user/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const { username, firstName, lastName, email, authToken } = req.body
+  try {
+    const user = await db.updateUser(id, {
+      username,
+      firstName,
+      lastName,
+      email,
+      authToken,
+    })
+    res.json(user)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+export default router
