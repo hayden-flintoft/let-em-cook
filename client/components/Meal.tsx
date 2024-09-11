9f21f46d2fe19974c14715615483a41788a9d55a
Ted Rainger <edward.rainger@gmail.com>
2024-09-11 15:10:42 +1200

    Finish meal card for now

--- client/components/Meal.tsx
+++ client/components/Meal.tsx
@@ -16,79 +16,89 @@ import {
 } from '@/components/ui/select'
 import { Checkbox } from '@/components/ui/checkbox'
 
-const ingredients = [
-  { id: 1, text: '1/2 tbsp olive oil' },
-  { id: 2, text: '100g / 3 oz salami stick, cut into 3mm / 1/8" thick rounds' },
-  {
-    id: 3,
-    text: '2 x 305g / 8 oz chicken breasts (large), cut in half horizontally',
-  },
-  { id: 4, text: '1/2 tsp cooking salt / kosher salt' },
-  { id: 5, text: '1/4 tsp black pepper' },
-  { id: 6, text: '1/2 tsp garlic powder' },
-  { id: 7, text: '3/4 tsp paprika' },
-  { id: 8, text: '1/4 tsp sage powder (optional)' },
-  { id: 9, text: '2 garlic cloves, finely minced' },
-  { id: 10, text: '1/2 onion, finely chopped' },
-]
+const meals = {
+  idMeal: '52772',
+  strMeal: 'Teriyaki Chicken Casserole',
+  strDrinkAlternate: null,
+  strCategory: 'Chicken',
+  strArea: 'Japanese',
+  strInstructions:
+    'Preheat oven to 350° F. Spray a 9x13-inch baking pan with non-stick spray.\r\nCombine soy sauce, ½ cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling.\r\nMeanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat.\r\nPlace the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks.\r\n*Meanwhile, steam or cook the vegetables according to package directions.\r\nAdd the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!',
+  strMealThumb:
+    'https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg',
+  strTags: 'Meat,Casserole',
+  strYoutube: 'https://www.youtube.com/watch?v=4aZr5hZXP_s',
+  ingredients: [
+    'soy sauce',
+    'water',
+    'brown sugar',
+    'ground ginger',
+    'minced garlic',
+  ],
+  strMeasure1: '3/4 cup',
+  strMeasure2: '1/2 cup',
+  strMeasure3: '1/4 cup',
+  strMeasure4: '1/2 teaspoon',
+  strMeasure5: '1/2 teaspoon',
+  strMeasure6: '4 Tablespoons',
+  strMeasure7: '2',
+  strMeasure8: '1 (12 oz.)',
+  strMeasure9: '3 cups',
+  strMeasure10: '',
+  strMeasure11: '',
+  strMeasure12: '',
+  strMeasure13: '',
+  strMeasure14: '',
+  strMeasure15: '',
+  strMeasure16: null,
+  strMeasure17: null,
+  strMeasure18: null,
+  strMeasure19: null,
+  strMeasure20: null,
+  strSource: null,
+  strImageSource: null,
+  strCreativeCommonsConfirmed: null,
+  dateModified: null,
+}
 
 export default function CardWithForm() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Card className="w-[60%] max-w-[1000px] p-4 shadow-lg">
         <CardHeader>
-          <p className="scroll-m-20 text-4xl font-extrabold tracking-tight text-[#9E3700] lg:text-5xl">
-            Recipe
+          <p className="scroll-m-20 text-3xl font-extrabold tracking-tight text-[#9E3700]">
+            {meals.strMeal}
           </p>
-          <CardDescription className="text-[#9E3700]">
-            Deploy your new project in one-click.
-          </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-[#9E3700]">
-                  Recipe Name
-                </Label>
-                <img src="images/Meals-by-Chefkraft.png" alt="food" />
+                <img
+                  src={meals.strMealThumb}
+                  alt="food"
+                  className="mx-auto w-[60%] object-cover"
+                />
+                <p className="scroll-m-20 text-xl font-extrabold tracking-tight text-[#9E3700]">
+                  {' '}
+                  {meals.strArea}
+                </p>
                 <div className="container mx-auto p-4">
-                  {ingredients.map((ingredient) => (
-                    <div
-                      key={ingredient.id}
-                      className="flex items-center space-x-2"
-                    >
+                  {/* Map over ingredients */}
+                  {meals.ingredients.map((ingredient, index) => (
+                    <div key={index} className="flex items-center space-x-2">
                       <Checkbox />
-                      <p>{ingredient.text}</p>
+                      {/* Combine the ingredient with its measurement */}
+                      <p>
+                        {meals[`strMeasure${index + 1}`]} {ingredient}
+                      </p>
                     </div>
                   ))}
                 </div>
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework" className="text-[#9E3700]">
-                  Cuisine
-                </Label>
-                <Select>
-                  <SelectTrigger id="framework">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Next.js</SelectItem>
-                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                    <SelectItem value="astro">Astro</SelectItem>
-                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
             </div>
           </form>
         </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline" className="border-[#9E3700] text-[#9E3700]">
-            Cancel
-          </Button>
-          <Button className="bg-[#9E3700] text-white">Deploy</Button>
-        </CardFooter>
       </Card>
     </div>
   )
