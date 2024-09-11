0d3e286d3e8c8ea423d93944a929ab132321170a
Hayden <84941902+hayden-flintoft@users.noreply.github.com>
2024-09-12 11:23:41 +1200

    Complete first iteration of returning an array of ingredients objects.

--- server/api/ingredients.ts
+++ server/api/ingredients.ts
@@ -38,4 +38,24 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET list of the ingredients for a recipe?
+// router.get('/', async (req, res) => {
+//   try {
+//     const response = await request.get(
+//       `https://www.themealdb.com/api/json/v2/${process.env.MEALDB_API_KEY}/list.php?i=list`,
+//     )
+
+//     if (!response.body || !response.body.meals) {
+//       throw new Error('Invalid API response')
+//     }
+
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.status(500).send(error.message)
+//     } else {
+//       res.status(500).send('Something went wrong')
+//     }
+//   }
+// })
+
 export default router
