0902926fc881f36b45c78df2279fba7017a0f455
Hayden Flintoft <hflintoft@gmail.com>
2024-09-18 08:43:59 +1200

    Fix image location

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -2,25 +2,25 @@ const teamMembers = [
   {
     name: 'Edward Soung',
     role: 'Product Owner',
-    img: 'images/Eddie.png',
+    img: '/images/Eddie.png',
     GitHub: 'https://github.com/EddieWeddie11',
   },
   {
     name: 'Hayden Flintoft',
     role: 'Product Owner / Backend Engineer',
-    img: 'images/Hayden.png',
+    img: '/images/Hayden.png',
     GitHub: 'https://github.com/hayden-flintoft',
   },
   {
     name: 'Edward Rainger',
     role: 'Frontend Engineer',
-    img: 'images/Ted.png',
+    img: '/images/Ted.png',
     GitHub: 'https://github.com/edward-rainger',
   },
   {
     name: 'Karl Bloomfield',
     role: 'Backend Engineer',
-    img: 'images/Karl.png',
+    img: '/images/Karl.png',
     GitHub: 'https://github.com/karl-bloomfield',
   },
 ]
@@ -29,30 +29,45 @@ const AboutUs = () => {
   return (
     <div className="about-us px-6 py-12">
       {/* Our Story Section */}
-      <section className="px-8 py-12 bg-white">
-  <h1 className="mb-6 text-5xl font-bold text-[#9E3700] leading-tight">
-  We believe that creativity in the kitchen does not need to come with a long grocery list.
-  </h1>
-  <br></br>
-  <p className="mb-10 text-xl text-[#9E3700]0">
-     Born from the idea that you can make the most of what you already have, our app helps you transform a few ingredients from your pantry or fridge into a delicious, satisfying meal. How often have you found yourself staring at random ingredients, unsure of what to cook? That is exactly the problem we set out to solve. We understand that not everyone has the time or energy to shop for complex recipes or follow long ingredient lists. With Let &apos;em Cook, we want to make cooking simple, fun, and accessible for everyone, no matter your skill level.
-  </p>
+      <section className="bg-white px-8 py-12">
+        <h1 className="mb-6 text-5xl font-bold leading-tight text-[#9E3700]">
+          We believe that creativity in the kitchen does not need to come with a
+          long grocery list.
+        </h1>
+        <br></br>
+        <p className="text-[#9E3700]0 mb-10 text-xl">
+          Born from the idea that you can make the most of what you already
+          have, our app helps you transform a few ingredients from your pantry
+          or fridge into a delicious, satisfying meal. How often have you found
+          yourself staring at random ingredients, unsure of what to cook? That
+          is exactly the problem we set out to solve. We understand that not
+          everyone has the time or energy to shop for complex recipes or follow
+          long ingredient lists. With Let &apos;em Cook, we want to make cooking
+          simple, fun, and accessible for everyone, no matter your skill level.
+        </p>
 
-  <h2 className="mb-4 text-4xl font-extrabold text-[#9E3700]">
-    Our Mission
-  </h2>
-  <p className="mb-10 text-lg text-gray-600">
-    Our mission is to inspire you to use what you have and reduce food waste, all while still enjoying flavorful, home-cooked meals. Whether you are a busy professional, a student, or someone looking to save money by using up ingredients before they spoil, Let &apos;em Cook is here to help you make the most out of every ingredient.
-  </p>
+        <h2 className="mb-4 text-4xl font-extrabold text-[#9E3700]">
+          Our Mission
+        </h2>
+        <p className="mb-10 text-lg text-gray-600">
+          Our mission is to inspire you to use what you have and reduce food
+          waste, all while still enjoying flavorful, home-cooked meals. Whether
+          you are a busy professional, a student, or someone looking to save
+          money by using up ingredients before they spoil, Let &apos;em Cook is
+          here to help you make the most out of every ingredient.
+        </p>
 
-  <h2 className="mb-4 text-4xl font-extrabold text-[#9E3700]">
-    How it Works
-  </h2>
-  <p className="mb-10 text-lg text-[#9E3700]">
-    Simply enter the ingredients you have on hand, and we will provide you with quick, easy, and creative meal ideas. The app is designed to be intuitive, offering recipe suggestions that fit your lifestyle—whether you are in the mood for something fast and simple or a bit more adventurous.
-  </p>
-</section>
-
+        <h2 className="mb-4 text-4xl font-extrabold text-[#9E3700]">
+          How it Works
+        </h2>
+        <p className="mb-10 text-lg text-[#9E3700]">
+          Simply enter the ingredients you have on hand, and we will provide you
+          with quick, easy, and creative meal ideas. The app is designed to be
+          intuitive, offering recipe suggestions that fit your lifestyle—whether
+          you are in the mood for something fast and simple or a bit more
+          adventurous.
+        </p>
+      </section>
 
       {/* All Members Section */}
       <section className="all-members text-center">
--- client/components/User.tsx
+++ client/components/User.tsx
@@ -1,95 +1,98 @@
-import { useState, useEffect } from 'react';
-import { useClerk, useUser } from '@clerk/clerk-react';
-import { Heart } from 'lucide-react';
-import { fetchRecipeById } from '@/api/recipes';
-import { Meal } from 'models/meals';
-
+import { useState, useEffect } from 'react'
+import { useClerk, useUser } from '@clerk/clerk-react'
+import { Heart } from 'lucide-react'
+import { fetchRecipeById } from '@/api/recipes'
+import { Meal } from 'models/meals'
 
 export default function User() {
-  const { user } = useUser();
-  const { openUserProfile } = useClerk();
-  const [likedRecipes, setLikedRecipes] = useState([] as Meal[]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [selectedRecipeId, setSelectedRecipeId] = useState(null);
+  const { user } = useUser()
+  const { openUserProfile } = useClerk()
+  const [likedRecipes, setLikedRecipes] = useState([] as Meal[])
+  const [isLoading, setIsLoading] = useState(true)
+  const [selectedRecipeId, setSelectedRecipeId] = useState(null)
 
   useEffect(() => {
-    fetchLikedRecipes();
-  }, [user]);
+    fetchLikedRecipes()
+  }, [user])
 
   const fetchLikedRecipes = async () => {
-    if (!user) return;
-    setIsLoading(true);
+    if (!user) return
+    setIsLoading(true)
     try {
-      const response = await fetch(`/api/v1/likes/user/${user.id}`);
-      const data = await response.json();
-      const promises: Meal[] = data.map( async (recipe) => {
-      const response = await fetchRecipeById(recipe.recipeId)
-      return response
-      } ) 
+      const response = await fetch(`/api/v1/likes/user/${user.id}`)
+      const data = await response.json()
+      const promises: Meal[] = data.map(async (recipe) => {
+        const response = await fetchRecipeById(recipe.recipeId)
+        return response
+      })
       const fullrecipes = await Promise.all(promises)
       console.log(fullrecipes)
-      setLikedRecipes(fullrecipes);
+      setLikedRecipes(fullrecipes)
     } catch (error) {
-      console.error('Error fetching liked recipes:', error);
+      console.error('Error fetching liked recipes:', error)
     } finally {
-      setIsLoading(false);
+      setIsLoading(false)
     }
-  };
+  }
 
   const handleOpenUserProfile = () => {
-    openUserProfile();
-  };
+    openUserProfile()
+  }
 
   const handleRecipeClick = (recipeId) => {
-    setSelectedRecipeId(recipeId);
-  };
-
-  
+    setSelectedRecipeId(recipeId)
+  }
 
   return (
-    <div className="relative max-w-4xl mx-auto bg-[#9E3700] shadow-lg rounded-3xl overflow-hidden">
+    <div className="relative mx-auto max-w-4xl overflow-hidden rounded-3xl bg-[#9E3700] shadow-lg">
       <div className="p-6">
-        <h1 className="text-4xl font-bold text-white mb-6">Profile</h1>
-        <div className="flex items-center mb-6">
-          <div className="w-24 h-24 bg-[#9E3700] rounded-3xl flex items-center border-5 border-white justify-center text-white text-4xl font-bold mr-4">
-            <img src="images/Untitled design (1).png" alt="Chef Icon" />
+        <h1 className="mb-6 text-4xl font-bold text-white">Profile</h1>
+        <div className="mb-6 flex items-center">
+          <div className="border-5 mr-4 flex h-24 w-24 items-center justify-center rounded-3xl border-white bg-[#9E3700] text-4xl font-bold text-white">
+            <img src="/images/Untitled design (1).png" alt="Chef Icon" />
           </div>
           <div>
-            <h2 className="text-3xl font-semibold text-white">{user?.firstName}</h2>
+            <h2 className="text-3xl font-semibold text-white">
+              {user?.firstName}
+            </h2>
             <p className="text-white">{user?.username}</p>
             <button
               onClick={handleOpenUserProfile}
-              className="mt-2 px-4 py-2 border border-white text-white rounded-full hover:bg-green-600 hover:text-white transition-colors"
+              className="mt-2 rounded-full border border-white px-4 py-2 text-white transition-colors hover:bg-green-600 hover:text-white"
             >
               Settings
             </button>
           </div>
         </div>
-        <div className="flex mb-4">
+        <div className="mb-4 flex">
           <button className="text-white">
             <span className="flex items-center">
-              <Heart className="w-6 h-6 mr-2" />
+              <Heart className="mr-2 h-6 w-6" />
               Likes
             </span>
           </button>
         </div>
       </div>
 
-      <div className="border-b border-white w-full relative z-10"></div>
+      <div className="relative z-10 w-full border-b border-white"></div>
 
-      <div className="p-6 z-10 relative">
+      <div className="relative z-10 p-6">
         {isLoading ? (
           <p className="text-white">Loading liked recipes...</p>
         ) : (
           <div className="grid grid-cols-2 gap-4">
             {likedRecipes.map((recipe) => (
-              <div 
-                key={recipe.idMeal} 
-                className="bg-white p-4 rounded-lg shadow-md cursor-pointer"
+              <div
+                key={recipe.idMeal}
+                className="cursor-pointer rounded-lg bg-white p-4 shadow-md"
                 onClick={() => handleRecipeClick(recipe.idMeal)}
               >
-                <img src={recipe.strMealThumb} alt={recipe.strMeal} className="w-full h-40 object-cover rounded-lg" />
-                <h3 className="text-xl font-semibold mt-2">{recipe.strMeal}</h3>
+                <img
+                  src={recipe.strMealThumb}
+                  alt={recipe.strMeal}
+                  className="h-40 w-full rounded-lg object-cover"
+                />
+                <h3 className="mt-2 text-xl font-semibold">{recipe.strMeal}</h3>
               </div>
             ))}
           </div>
@@ -101,22 +104,26 @@ export default function User() {
       </div>
 
       {selectedRecipeId && (
-        <div className="p-6 bg-white rounded-lg mt-6">
-          <h2 className="text-2xl font-bold mb-4">Selected Recipe</h2>
+        <div className="mt-6 rounded-lg bg-white p-6">
+          <h2 className="mb-4 text-2xl font-bold">Selected Recipe</h2>
           {isRecipeLoading ? (
             <p>Loading recipe...</p>
           ) : selectedRecipe ? (
             <div>
               <h3 className="text-xl font-semibold">{selectedRecipe.name}</h3>
-              <img src={selectedRecipe.image} alt={selectedRecipe.name} className="w-full h-60 object-cover rounded-lg my-4" />
+              <img
+                src={selectedRecipe.image}
+                alt={selectedRecipe.name}
+                className="my-4 h-60 w-full rounded-lg object-cover"
+              />
               <p>{selectedRecipe.description}</p>
-              <h4 className="font-semibold mt-4">Ingredients:</h4>
+              <h4 className="mt-4 font-semibold">Ingredients:</h4>
               <ul className="list-disc pl-5">
                 {selectedRecipe.ingredients.map((ingredient, index) => (
                   <li key={index}>{ingredient}</li>
                 ))}
               </ul>
-              <h4 className="font-semibold mt-4">Instructions:</h4>
+              <h4 className="mt-4 font-semibold">Instructions:</h4>
               <ol className="list-decimal pl-5">
                 {selectedRecipe.instructions.map((instruction, index) => (
                   <li key={index}>{instruction}</li>
@@ -129,5 +136,5 @@ export default function User() {
         </div>
       )}
     </div>
-  );
+  )
 }
